c7b31ce781b4b4302376c925bec802c79115c696
Deepanshu Pal <deepanshupal@Deepanshus-MacBook-Air.local>
2019-10-26 19:09:05 +0530

    Armstrong Program Added

---
+++ Armstrong.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+bool checkArmstrongNumber(int num);
+
+int main(){
+    int num;
+    bool flag;
+    cout<<"Enter a positive  integer: ";
+    cin>>num;
+    
+    flag = checkArmstrongNumber(num);
+    if(flag == true)
+        cout<<num<<" is an Armstrong number.";
+    else
+        cout<<num<<" is not an Armstrong number.";
+    
+    return 0;
+}
+
+bool checkArmstrongNumber(int num) {
+    int temp, sum=0, digit;
+    bool isArm;
+    
+    temp = num;
+    while(temp != 0) {
+        digit = temp % 10;
+        sum = sum +(digit * digit * digit);
+        temp = temp/10;
+    }
+    if (sum==num)
+        isArm = true;
+    else
+        isArm = false;
+    
+    return isArm;
+}
